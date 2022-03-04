30e1184f78ccdff255cc186f92caa9772c45cbde
AsianJS <92735221+AsianJS@users.noreply.github.com>
2022-03-04 12:10:14 -0500

    3/4/2022

--- javascript.js
+++ javascript.js
@@ -1,8 +1,26 @@
-var quotes = {
-  'Why was 9 afraid of 7? - Because of 789!',
-  'What do you call a man with no arms or legs, laying in front of your door? - Matt!',
-}
+var quotes = [
+  'Why was 9 afraid of 7?',
+  'What do you call a man with no arms or legs, laying in front of your door?',
+  'Why do Nordic military ships have barcodes?',
+  'What did the dividing cell say when his sister stepped on his foot?',
+];
+var punchlines = [
+  "Because of 789!",
+  'Matt!',
+  'To scan-the-navy-in!',
+  'That\'s my toe sis!',
+];
+
+var randomNumber;
+var joke;
+function random(){
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  console.log(quotes[randomNumber]);
+};
 function newQuote(){
-  var randomNumber = Math.floor(Math.random() * (quote.length));
   document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
-}
+};
+function punchline(){
+  document.getElementById('quoteDisplay').innerHTML = punchlines[randomNumber];
+};
+var randomNumber = Math.floor(Math.random() * (quotes.length));
